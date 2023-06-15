9e151a9e26006f12b2fa66a33a48e18871209e82
Willow Yang <willowyang@dynamic-oit-vapornet100-10-8-88-232.princeton.edu>
2023-06-15 09:36:21 -0400

    listing card displays name of the item

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -38,7 +38,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
   const router = useRouter();
   const { getByValue } = useCountries();
 
-  const location = getByValue(data.locationValue);
+  //const location = getByValue(data.locationValue);
 
   const handleCancel = useCallback(
     (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -109,7 +109,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
           </div>
         </div>
         <div className="font-semibold text-lg">
-          {location?.region}, {location?.label}
+          {data.title}
         </div>
         <div className="font-light text-neutral-500">
           {reservationDate || data.category}
